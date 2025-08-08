7181e93767b3a5bff693c17106492e645a81725b
Yujin Kim <yujinkim0206@gmail.com>
2025-08-08 00:01:48 -0400

    preferences

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,9 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);
