3188d7c8ec9bfe30fc67177bbee5fefa76039217
Lucy Siyoung Lee <leelucyoung@gmail.com>
2025-09-20 18:50:34 -0400

    Feedback adjustment

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,61 +2,74 @@ export default function AboutPage() {
   return (
     <main className="bg-white text-gray-800">
       {/* Hero Section */}
-      <section className="relative h-[80vh] mt-[var(--header)]">
+      <section className="section-spacing relative h-[100vh] w-full overflow-hidden">
         <img
           src="/images/jason-uoft.jpg"
-          alt="University"
-          className="absolute inset-0 h-full w-full object-cover"
+          alt=""
+          className="h-full w-full object-cover"
         />
-        <div className="absolute inset-0 bg-black/40" />
-        <div className="relative z-10 h-full flex items-center justify-start px-6 md:px-20">
-          <div className="max-w-2xl bg-black/40 text-white p-6 rounded-sm">
-            <h1 className="text-4xl font-bold mb-4">About Us</h1>
-            <p className="text-lg leading-relaxed">
-              Founded in 1986, Association of Korean-Canadian Scientists and Engineers (AKCSE) is a
-              non-profit professional organization that promotes the exchange of scientific knowledge
-              and fosters cooperation between Korean and Canadian communities.
-              As the University of Toronto chapter, AKCSE UofT supports Korean-Canadian students in STEM through academic, professional, and cultural initiatives.
-            </p>
+        <div className="absolute inset-0 bg-gradient-to-r from-black/90 to-transparent">
+          <div className="mx-auto flex h-full max-w-[90rem] items-center justify-start px-6">
+            <div className="w-full text-white md:w-[40%]">
+              <h1 className="mb-6 text-5xl font-light">About Us</h1>
+              <p className="leading-7">
+                Founded in 1986, Association of Korean-Canadian Scientists and
+                Engineers (AKCSE) is a non-profit professional organization that
+                promotes the exchange of scientific knowledge and fosters
+                cooperation between Korean and Canadian communities. As the
+                University of Toronto chapter, AKCSE UofT supports
+                Korean-Canadian students in STEM through academic, professional,
+                and cultural initiatives.
+              </p>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className="mt-24 grid md:grid-cols-2 gap-24 items-start pb-24 px-6 md:px-30">
-      {/* 왼쪽: 이미지 두 장 */}
-      <div className="mt-10 flex gap-6 md:gap-10 pl-4 md:pl-10">
-        <img
-          src="/images/about-akcse.jpg"
-          alt="AKCSE UofT"
-          className="w-1/2 object-cover shadow-md"
-        />
-        <img
-          src="/images/about-coffeechat.jpg"
-          alt="Coffee Chat"
-          className="w-1/2 object-cover shadow-md"
-        />
-      </div>
-
-      {/* 오른쪽: 비전 설명 */}
-      <div>
-        <h2 className="text-3xl font-semibold mb-6">Our Vision</h2>
-        <p className="text-base leading-relaxed mb-4">
-         At AKCSE UofT, we envision a community where
-         Korean-Canadian students in STEM feel seen, supported, and inspired.
-        </p>
-        <p className="text-base leading-relaxed mb-4">
-          We strive to create a space where academic ambition and cultural identity grow side by side.
-          Through mentorship, skill-building, and peer support, we help students navigate their unique journeys.
-          We believe that true growth happens not only in classrooms, but also in collaboration, curiosity, and courage.
-        </p>
-        <p className="text-base leading-relaxed">
-          We are committed to building bridges—between disciplines, cultures, and generations.
-          By empowering individuals, we strengthen our collective voice and presence in STEM.
-          Together, we’re shaping a future where knowledge and connection lead the way.
-        </p>
-      </div>
-    </section>
-
+      <section className="section-spacing mx-auto flex w-[100vw] flex-col items-center justify-center px-6">
+        {/* 오른쪽: 비전 설명 */}
+        <div className="flex max-w-2xl flex-col gap-8 text-center">
+          <h2 className="mb-4 text-5xl leading-8 font-light">Our Vision</h2>
+          <p className="text-base leading-7">
+            At AKCSE UofT, we envision a community
+            <br /> where Korean-Canadian students in STEM feel seen, supported,
+            and inspired.
+          </p>
+          <p className="text-base leading-7">
+            We strive to create a space
+            <br /> where academic ambition and cultural identity grow side by
+            side. <br />
+            Through mentorship, skill-building, and peer support, <br />
+            we help students navigate their unique journeys. We believe that
+            true growth happens not only in classrooms, but also in
+            collaboration, curiosity, and courage.
+          </p>
+          <p className="text-base leading-7">
+            We are committed to building bridges
+            <br />
+            —between disciplines, cultures, and generations. <br />
+            By empowering individuals, <br />
+            we strengthen our collective voice and presence in STEM.
+          </p>
+          <p className="text-base leading-7">
+            Together, we’re shaping a future where knowledge and connection lead
+            the way.
+          </p>
+        </div>
+        <div className="mx-auto mt-20 flex max-w-7xl flex-col gap-10 md:flex-row md:justify-center">
+          <img
+            src="/images/about-akcse.jpg"
+            alt="AKCSE UofT"
+            className="w-full object-cover shadow-md md:w-1/3"
+          />
+          <img
+            src="/images/about-coffeechat.jpg"
+            alt="Coffee Chat"
+            className="w-full object-cover shadow-md md:w-1/3"
+          />
+        </div>
+      </section>
     </main>
   );
 }
+
--- app/members/page.tsx
+++ app/members/page.tsx
@@ -3,73 +3,73 @@ export default function MembersPage() {
     {
       title: "Presidency",
       members: [
-        { name: "Sumin Lee", role: "President", image: "/images/mon.png" },
-        { name: "Eric Lee", role: "Vice President", image: "/images/회장단_이상헌.jpeg" },
-        { name: "Paul Im", role: "Vice President", image: "/images/mon.png" },
+        { name: "Sumin Lee", role: "President", grade: "4th year", major: "Major in Mathematics & Statistics", image: "/images/회장단_이수민.png" },
+        { name: "Eric Lee", role: "Vice President", grade: "3rd year", major: "Major in Economics and Mathematics", image: "/images/회장단_이상헌.jpeg" },
+        { name: "Paul Im", role: "Vice President", grade: "3rd year", major: "Specialist in Pharmacology & Biomedical Toxicology", image: "/images/회장단_임현욱.jpg" },
       ],
     },
     {
       title: "Marketing",
       members: [
-        { name: "Ryan Baek", role: "Director", image: "/images/마케팅_백승주.jpeg" },
-        { name: "Jian Huh", role: "Director", image: "/images/mon.png" },
-        { name: "Sarah Park", role: "Executive Member", image: "/images/마케팅_박수현.jpg" },
-        { name: "Jenny Kim", role: "Executive Member", image: "/images/마케팅_김제니.jpeg" },
+        { name: "Ryan Baek", role: "Director", grade: "4th year", major: "Biological Physics speacialist - Advanced Physics stream", image: "/images/마케팅_백승주.jpeg" },
+        { name: "Jian Huh", role: "Director", grade: "Gap year", major: "Specialist in Architectural Studies", image: "/images/마케팅_허지안.jpg" },
+        { name: "Sarah Park", role: "Executive Member", grade: "1st year", major: "Major not claimed yet", image: "/images/마케팅_박수현.jpg" },
+        { name: "Jenny Kim", role: "Executive Member", grade: "2nd year", major: "Major in Health and Disease & Animal Physiology, Minor in Statistics", image: "/images/마케팅_김제니.jpeg" },
       ],
     },
     {
       title: "Finance",
       members: [
-        { name: "Yoon Park", role: "Director", image: "/images/mon.png" },
-        { name: "Hanseo Park", role: "Director", image: "/images/mon.png" },
-        { name: "Jinwoo Choi", role: "Executive Member", image: "/images/mon.png"},
-        { name: "Daeun Lee", role: "Executive Member", image: "/images/mon.png" },
-        { name: "Yerin Lee", role: "Executive Member", image: "/images/파이낸스_이예린.jpg"  },
+        { name: "Yoon Park", role: "Director", grade: "4th year", major: "Major in Mathematics & Statistics", image: "/images/파이낸스_박윤서.jpg" },
+        { name: "Hanseo Park", role: "Director", grade: "3rd year", major: "Major in Mathematics ", image: "/images/파이낸스_박한서.png" },
+        { name: "Jinwoo Choi", role: "Executive Member", grade: "3rd year", major: "Major in Mathematics & Drama", image: "/images/mon.png"},
+        { name: "Daeun Lee", role: "Executive Member", grade: "4th year", major: "Major in Human Geography, Minor in Environmentel Geography, East Asain Studies", image: "/images/파이낸스_이다은.jpeg" },
+        { name: "Yerin Lee", role: "Executive Member", grade: "2nd year", major: "Major in Chemical Engineering", image: "/images/파이낸스_이예린.jpg"  },
       ],
     },
     {
       title: "Educational Events",
       members: [
-        { name: "Sylvia Park", role: "Director", image: "/images/에듀이벤트_박하연.jpg" },
-        { name: "Huiwon Shin", role: "Director", image: "/images/에듀이벤트_신희원.jpg" },
-        { name: "Chaewon Jin", role: "Executive Member", image: "/images/mon.png"},
-        { name: "Aimie Kang", role: "Executive Member", image: "/images/에듀이벤트_강에이미.jpg" },
-        { name: "Heejin Kim", role: "Executive Member", image: "/images/에듀이벤트_김희진.jpg" },
-        { name: "Min Kim", role: "Executive Member", image: "/images/에듀이벤트_김민호.jpg" }
+        { name: "Sylvia Park", role: "Director", grade: "3rd year", major: "Major in Global Health & French Language and French Linguistics, minor in Immunology", image: "/images/에듀이벤트_박하연.jpg" },
+        { name: "Huiwon Shin", role: "Director", grade: "3rd year", major: "Major in Pharmacology, Minor in Immunology & Physiology", image: "/images/에듀이벤트_신희원.jpg" },
+        { name: "Chaewon Jin", role: "Executive Member", grade: "3rd year", major: "Major in Life Sciences", image: "/images/에듀이벤트_진채원.jpg"},
+        { name: "Aimie Kang", role: "Executive Member", grade: "3rd year", major: "Major in Global Health & Bioethics, Minor in Environmental Ethics", image: "/images/에듀이벤트_강에이미.jpg" },
+        { name: "Heejin Kim", role: "Executive Member", grade: "3rd year", major: "Major in Biomedical Toxicology & Physiology, Minor in Immunology", image: "/images/에듀이벤트_김희진.jpg" },
+        { name: "Min Kim", role: "Executive Member", grade: "2nd year", major: "Specialist in physics, Minor in Mathematics", image: "/images/에듀이벤트_김민호.jpg" }
       ],
     },
     {
       title: "Social Events",
       members: [
-        { name: "Ashely Chang", role: "Director", image: "/images/소셜이벤트_장다정.jpg" },
-        { name: "Jiyu Chae", role: "Director", image: "/images/mon.png"  },
-        { name: "Jung Min Son", role: "Executive Member", image: "/images/소셜이벤트_손정민.jpg"  },
-        { name: "Rian Jeong", role: "Executive Member", image: "/images/소셜이벤트_정현진.jpg"  },
-        { name: "Daniel Yoo", role: "Executive Member", image: "/images/소셜이벤트_유종상.jpeg"  },
+        { name: "Ashely Chang", role: "Director", grade: "3rd year", major: "Major in Human Biology, Minor in Immunology and Physiology", image: "/images/소셜이벤트_장다정.jpg" },
+        { name: "Jiyu Chae", role: "Director", grade: "4th year", major: "Major in Pharmacology & Cell and Molecular Biology", image: "/images/소셜이벤트_채지유.jpg"  },
+        { name: "Jung Min Son", role: "Executive Member", grade: "2nd year", major: "Major in Quantitative Biology, Minor in Mathematics & Applied Data Science", image: "/images/소셜이벤트_손정민.jpg"  },
+        { name: "Rian Hyeonjin Jeong", role: "Executive Member", grade: "2nd year", major: "Major in Management and Accounting ", image: "/images/소셜이벤트_정현진.jpg"  },
+        { name: "Daniel Yoo", role: "Executive Member", grade: "3rd year", major: "Major in Mechanical Engineering", image: "/images/소셜이벤트_유종상.jpeg"  },
       ],
     },
     {
       title: "Programming",
       members: [
-        { name: "Yujin Kim", role: "Director", image: "/images/프로그래밍_김유진.jpeg"  },
-        { name: "Daniel Kim", role: "Executive Member", image: "/images/mon.png" },
-        { name: "Lucy Lee", role: "Executive Member", image: "/images/프로그래밍_이시영.jpg" },
+        { name: "Yujin Kim", role: "Director", grade: "3rd year", major: "Major in Computer Science & Statistics", image: "/images/프로그래밍_김유진.jpeg"  },
+        { name: "Daniel Kim", role: "Executive Member", grade: "2nd year", major: "Specialist in Mathematics & Its Applications (Probability/Statistics), Minor in Applied Data Science", image: "/images/프로그래밍_김의찬.jpeg" },
+        { name: "Lucy Lee", role: "Executive Member", grade: "2nd year", major: "Major in Mathematics and New Media Studies, Minor in Statistics", image: "/images/프로그래밍_이시영.jpg" },
       ],
     },
     {
       title: "Research & Literature",
       members: [
-        { name: "Yoon Kang", role: "Director", image: "/images/mon.png"  },
-        { name: "Ellie Jung", role: "Director", image: "/images/리서치_정여진.jpeg" },
-        { name: "Christine Seo", role: "(Life Science) Executive Member",image: "/images/mon.png" },
-        { name: "Lucas Yoon", role: "(Tech) Executive Member", image: "/images/리서치_윤준서.jpg"  },
-        { name: "David Shin", role: "(Tech) Executive Member", image: "/images/리서치_신승민.jpg"  },
+        { name: "Yoon Kang", role: "Director", grade: "4th year", major: "Major in Physiology and Health & Disease", image: "/images/mon.png"  },
+        { name: "Ellie Jung", role: "Director", grade: "Gap term", major: "Major in Industrial Engineering", image: "/images/리서치_정여진.jpeg" },
+        { name: "Christine Seo", role: "(Life Science) Executive Member", grade: "3rd year", major: "Major in Neuroscience & Nutritional Science, Minor in Immunology ", image: "/images/mon.png" },
+        { name: "Lucas Yoon", role: "(Tech) Executive Member", grade: "2nd year", major: "Major in Mathematics and Statistics", image: "/images/리서치_윤준서.jpg"  },
+        { name: "David Shin", role: "(Tech) Executive Member", grade: "2nd year", major: "Specialist in Mathematics & Its applications (Probability/Statistics), Minor in Statistics", image: "/images/리서치_신승민.jpg"  },
       ],
     },
   ];
 
   const MemberCard = ( { member }: { member: any }) => (
-    <div className="w-60 bg-white p-4 shadow-sm hover:shadow-md transition text-center">
+    <div className="w-60 bg-white p-4 shadow-md text-center">
       {member.image && (
         <img
           src={member.image}
@@ -77,16 +77,19 @@ export default function MembersPage() {
           className="w-45 h-55 object-cover mx-auto mb-2"
         />
       )}
-      <p className="font-bold text-lg">{member.name}</p>
-      <p className="text-md text-gray-600">{member.role}</p>
+      <p className="text-md">{member.name}</p>
+      <p className="text-sm text-[var(--subtitle)]">{member.role}</p>
+      <p className="text-sm text-[var(--subtitle)]">{member.grade}</p>
+      <p className="text-sm text-[var(--subtitle)]">{member.major}</p>
+
     </div>
   );
 
   return (
-    <main className="bg-white text-gray-800">
-      <section className="bg-[#1E3765] pt-28 pb-20 text-center px-6 md:px-20">
-          <h1 className="text-5xl font-extrabold text-white mt-20 mb-2">OUR TEAM</h1>
-          <p className="text-white font-semibold text-lg px-20 max-w-5xl mx-auto">
+    <main className="bg-white">
+      <section className="pt-28 pb-20 text-center px-6 md:px-20">
+          <h1 className="text-5xl font-light-mb-6 text-black mt-20 mb-2">Our Team</h1>
+          <p className="text-black text-lg px-6 max-w-5xl mx-auto">
             AKCSE UofT is run by passionate student leaders committed to supporting the
             academic, professional, and personal growth of Korean-Canadian students in STEM.
           </p>
@@ -98,10 +101,10 @@ export default function MembersPage() {
           const executives = section.members.filter((m) => m.role !== "President" && m.role !== "Director");
           
           return (
-            <div key={section.title} className="max-w-6xl mx-auto text-center space-y-8 bg-[#eef3fa] p-8 rounded-xl shadow-md">
-              <h2 className="text-4xl font-semibold mb-6 text-center">
+            <div key={section.title} className="max-w-6xl mx-auto text-center space-y-8 bg-gray-100 p-8">
+              <h2 className="text-3xl mb-6 text-center">
                 <span className="relative inline-block">
-                <span className="absolute inset-0 bg-blue-200 rounded-md -z-10 w-full h-full" />
+                <span className="absolute inset-0 bg-blue-200 -z-10 w-full h-full" />
                 {section.title}
                 </span>
               </h2>
