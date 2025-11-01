b815e4decab63649c7c9ea15cae3f213a40242d1
Daniel (Euichan) Kim <93475698+danielkim-im@users.noreply.github.com>
2025-11-01 15:26:23 -0400

    Fix formatting issue in ImageBlock component

--- app/research/[id]/ArticleBlocks/ImageBlock.tsx
+++ app/research/[id]/ArticleBlocks/ImageBlock.tsx
@@ -12,7 +12,7 @@ export default function ImageBlock({
         className="mx-auto max-w-[75%] bg-gray-200 object-cover lg:aspect-auto"
       />
       <p className="mt-4 text-center leading-7 italic sm:mt-6">
-        {image_description}
+        {image_description} 
       </p>
     </div>
   );
